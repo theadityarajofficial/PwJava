9a1abf26a5c307727c797cb519282e282037a66d
theadityarajofficial <rajaditya75571@gmail.com>
2023-12-15 15:48:43 +0530

    updated

---
+++ Array/length_keyword.java
@@ -0,0 +1,6 @@
+public class length_keyword {
+    public static void main(String[] args) {
+        int arr[]={5,4,7,8};
+        System.out.println(arr.length);
+    }
+}
---
+++ Quiz/quiz.java
@@ -0,0 +1,11 @@
+public class quiz{
+
+        public static void main(String args[]){
+
+                int[] a=new int[0];
+
+                System.out. println(a.length);
+
+        }
+
+}
