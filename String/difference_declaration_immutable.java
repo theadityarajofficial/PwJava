06c4e076601d5eaed8b3ee22c4f44534ca86e2d4
theadityarajofficial <rajaditya75571@gmail.com>
2023-12-16 12:30:16 +0530

    updated

--- String/difference_declaration_immutable.java
+++ String/difference_declaration_immutable.java
@@ -1,12 +1,15 @@
 public class difference_declaration_immutable {
     public static void main(String[] args) {
-        String S1="PW";
-        String S2="PW";
+        String S1="PWSKILLS";
+        String S2="PWSKILLS";
         String S3=new String("PWSKILLS");
         String S4=new String("PWSKILLS");
+        String S5="PwSkills";
         System.out.println(S1==S2);
         System.out.println(S3==S4);
         String s=new String();
         System.out.println(s);
+        System.out.println(S1.equals(S3));
+        System.out.println(S1.equals(S5));
     }
 }
