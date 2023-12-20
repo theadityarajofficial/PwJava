5fd0d7e29ad728ecb9dfa215d0644e5f44936f5d
theadityarajofficial <rajaditya75571@gmail.com>
2023-12-21 01:24:49 +0530

    updated

--- Static/Static_Basics.java
+++ Static/Static_Basics.java
@@ -1,52 +1,65 @@
 public class Static_Basics {
 
-     static int age;
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        disp();
-        DemoStatic d=new DemoStatic();
-        DemoStatic.disp1();
-        d.disp2();
-        d.disp1();
-//        DemoStatic.disp2();
-    }
-
+     static int age=20;
 
     static
     {
         age =10;
-        System.out.println("Static block");
+        System.out.println("Control in Static block");
     }
 
-    static void disp(){
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Static_Basics sb=new Static_Basics();
+        sb.disp();
+        DemoStatic d=new DemoStatic();
+//        DemoStatic d1=new DemoStatic();
+        DemoStatic.disp1();
+        d.disp2();
+        d.disp1();
+        d.disp3();
+
+//        DemoStatic.disp2();
+    }
+
+
+    void disp(){
         {
-            System.out.println(25);
+            System.out.println(age);
         }
     }
-    static {
-        disp();
-    }
 
-    int age1=5;
-    {
-        System.out.println(age1);
-    }
-
-    static {
-        DemoStatic.disp1();
-    }
+//    static {
+//        DemoStatic.disp1();
+//    }
 
 }
-class DemoStatic{
-    int age=20;
+ class DemoStatic{
 
-    static void disp1(){
-        DemoStatic ds=new DemoStatic();
-        System.out.println(ds.age);
+    int m,n;
+    {
+        m=10;
+        n=30;
+        System.out.println("control in non static or initialization block");
+//        instance
+//        every time object is created it consumes memory
+    }
+    void disp3(){
+        System.out.println("value of instance ver "+m);
+        System.out.println("value of instance var "+n);
+    }
+
+    static int age;
+    static
+    {
+        age=18; //java initialization block
+    }
+
+     static void disp1(){
+        System.out.println("age "+age);
     }
     void disp2(){
+        System.out.println(age);
         System.out.println("New disp");
     }
 }
